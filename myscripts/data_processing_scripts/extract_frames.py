881ca93a3fb58e726a4d164b2cdc7a9d865ebdc0
MarcusNerva <hadrianus_1@163.com>
2020-09-18 00:46:04 +0800

    debug

--- myscripts/data_processing_scripts/extract_frames.py
+++ myscripts/data_processing_scripts/extract_frames.py
@@ -68,6 +68,7 @@ if __name__ == '__main__':
 
     for i, (video_path) in enumerate(video_list):
         extract_frames(video_path, dst)
+        dst = sorted(dst)
         frames_list = glob.glob(os.path.join(dst, '*.jpg'))
         dpp = DPPModel(mode, dst, settings, device, _mobilenet)
         Yg = dpp.dpp()
