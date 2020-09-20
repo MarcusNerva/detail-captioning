aeb2e3c1d77ed79b33deca683bbd39a74bad736a
MarcusNerva <hadrianus_1@163.com>
2020-09-20 11:03:36 +0800

    debug

--- myscripts/data_processing_scripts/shell_scripts/get_2d_3d_feats.sh
+++ myscripts/data_processing_scripts/shell_scripts/get_2d_3d_feats.sh
@@ -39,8 +39,11 @@ conda activate py37
 # python /home/hanhuaye/PythonProject/opensource/MSDN/merge_boxes.py || exit 1
 # echo "===============merging boxes is finished==============="
 
-python /home/hanhuaye/PythonProject/detail-captioning/myscripts/data_processing_scripts/rearrange_object.py || exit 1
-echo "===============rearranging objects & relation feats is finished==============="
+python /home/hanhuaye/PythonProject/detail-captioning/myscripts/data_processing_scripts/merge_scene_feats.py || exit 1
+echo "===============merging res2d_feats is finished==============="
+
+# python /home/hanhuaye/PythonProject/detail-captioning/myscripts/data_processing_scripts/rearrange_object.py || exit 1
+# echo "===============rearranging objects & relation feats is finished==============="
 
 # python /home/hanhuaye/PythonProject/detail-captioning/myscripts/data_processing_scripts/pick_up_i3d_feats.py || exit 1
 # echo "===============picking up i3d feats is finished==============="
