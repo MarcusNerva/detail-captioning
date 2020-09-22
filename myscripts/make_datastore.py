eee8f4d12fc982e51c8760e7e37cc99dc3382182
MarcusNerva <hadrianus_1@163.com>
2020-09-22 10:45:10 +0800

    fix memory explosion

--- myscripts/make_datastore.py
+++ myscripts/make_datastore.py
@@ -5,6 +5,7 @@ import torch
 import torchtext
 import os
 import pickle
+import gc
 
 def make_datastore(args, mode, data_range):
     if not mode in ['train', 'valid', 'test']:
@@ -41,37 +42,48 @@ def make_datastore(args, mode, data_range):
         seq_mask = pickle.load(f)
 
     for vid in data_range:
-        res2d_path = os.path.join(res2d_dir, vid + '.npy')
-        i3d_path = os.path.join(i3d_dir, vid + '.npy')
-        relation_path = os.path.join(relation_dir, vid + '.npy')
-        object_path = os.path.join(object_dir, vid + '.npy')
-
-        res2d = np.load(res2d_path)
-        i3d = np.load(i3d_path)
-        relation = np.load(relation_path)
-        object_ = np.load(object_path)
-
+        res2d_single_path = os.path.join(res2d_dir, vid + '.npy')
+        res2d = np.load(res2d_single_path)
         res2d_dict[vid] = res2d
-        i3d_dict[vid] = i3d
-        relation_dict[vid] = relation
-        object_dict[vid] = object_
+    with open(res2d_path, 'wb') as f:
+        pickle.dump(res2d_dict, f)
+    del res2d_dict
+    gc.collect()
 
+    for vid in data_range:
+        i3d_single_path = os.path.join(i3d_dir, vid + '.npy')
+        i3d = np.load(i3d_single_path)
+        i3d_dict[vid] = i3d
+    with open(i3d_path, 'wb') as f:
+        pickle.dump(i3d_path, f)
+    del i3d_dict
+    gc.collect()
+
+    for vid in data_range:
+        relation_single_path = os.path.join(relation_dir, vid + '.npy')
+        relation = np.load(relation_single_path)
+        relation_dict[vid] = relation
+    with open(relation_path, 'wb') as f:
+        pickle.dump(relation_dict, f)
+    del relation_dict
+    gc.collect()
+
+    for vid in data_range:
+        object_single_path = os.path.join(object_dir, vid + '.npy')
+        object_ = np.load(object_single_path)
+        object_dict[vid] = object_
+    with open(object_path, 'wb') as f:
+        pickle.dump(object_dict, f)
+    del object_dict
+    gc.collect()
+
+    for vid in data_range:
         for number_tensor in numberic_dict[vid]:
             numberic.append(number_tensor)
             video_id.append(vid)
         for mask in seq_mask[vid]:
             word_mask.append(mask)
-        
-        print('{vid} is finished'.format(vid=vid))
 
-    with open(res2d_path, 'wb') as f:
-        pickle.dump(res2d_dict, f)
-    with open(i3d_path, 'wb') as f:
-        pickle.dump(i3d_dict, f)
-    with open(relation_path, 'wb') as f:
-        pickle.dump(relation_dict, f)
-    with open(object_path, 'wb') as f:
-        pickle.dump(object_dict, f)
     with open(numberic_path, 'wb') as f:
         pickle.dump(numberic, f)
     with open(video_id_path, 'wb') as f:
@@ -79,3 +91,4 @@ def make_datastore(args, mode, data_range):
     with open(word_mask_path, 'wb') as f:
         pickle.dump(word_mask, f)
 
+    
